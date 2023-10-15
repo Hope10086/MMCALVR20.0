d81d668a5c84c265b72852b5db4c845f77d11786
Hope <1554061588@qq.com>
2023-10-15 16:32:17 +0800

    GLSL parameter Input
    GLSL parameter Input set ， seven Gaussian Kernel

--- alvr/client_core/cpp/GaussianBlurPass.cpp
+++ alvr/client_core/cpp/GaussianBlurPass.cpp
@@ -11,55 +11,57 @@ using namespace std;
 using namespace gl_render_utils;
 
 namespace {
-const string HORIZONTAL_BLUR_SHADER = R"glsl(#version 300 es
+
+const string BLUR_COMMON_SHADER_FORMAT = R"glsl(#version 300 es
     #extension GL_OES_EGL_image_external_essl3 : enable
-    precision mediump float;
+    precision highp float;
+
+    const vec2 TEXTURE_SIZE = vec2(%i,%i);
+    const float a = (%f);
+    const float b = (%f);
+    const float c = (%f);
+    float kernel[5] = float[5](a, b, c, b ,a);
+    const float kernelWeight = 6.0;
+
+
+    )glsl";
+
+const string HORIZONTAL_BLUR_SHADER = R"glsl(
+    
     in vec2 uv;
     out vec4 fragColor;
-    
     uniform sampler2D inputTexture;
-    const float kernel[7] = float[7](1.0, 1.0, 1.0, 0.5 ,1.0 ,1.0 ,1.0);
-    const float kernelWeight = 7.0;
-    const int uTextureWidth = 5184 ; 
-    const int uTextureHeight = 2592; 
 
     void main() {
         vec4 color = texture(inputTexture, uv);
-        vec3 result = color.rgb * kernel[3];
-       for (int i = -3; i <= 3; i++) {
+        vec3 result = color.rgb * kernel[2];
+       for (int i = -2; i <= 2; i++) {
         ivec2 offset = ivec2(i, 0);
  
-        vec4 nearsample = texture(inputTexture, uv + vec2(offset.x, 0.0) / vec2(uTextureWidth, uTextureHeight));
+        vec4 nearsample = texture(inputTexture, uv + vec2(offset.x, 0.0) / TEXTURE_SIZE);
 
-        result += nearsample.rgb * kernel[i + 3];
+        result += nearsample.rgb * kernel[i + 2];
         }
         fragColor = vec4(result / kernelWeight, color.a);
     }
 )glsl";
 
 
+const string  VERTICAL_BLUR_SHADER = R"glsl(
 
-const string  VERTICAL_BLUR_SHADER = R"glsl(#version 300 es
-    #extension GL_OES_EGL_image_external_essl3 : enable
-    precision mediump float;
     in vec2 uv;
     out vec4 fragColor;
-    
     uniform sampler2D inputTexture;
-    const float kernel[7] = float[7](1.0, 1.0, 1.0, 0.5 ,1.0 ,1.0 ,1.0);
-    const float kernelWeight = 7.0;
-    const int uTextureWidth = 5184 ; 
-    const int uTextureHeight = 2592; 
 
     void main() {
         vec4 color = texture(inputTexture, uv);
-        vec3 result = color.rgb * kernel[3];
-       for (int i = -3; i <= 3; i++) {
+        vec3 result = color.rgb * kernel[2];
+       for (int i = -2; i <= 2; i++) {
         ivec2 offset = ivec2(0, i);
  
-        vec4 nearsample = texture(inputTexture, uv + vec2(0.0 ,offset.y) / vec2(uTextureWidth, uTextureHeight));
+        vec4 nearsample = texture(inputTexture, uv + vec2(0.0 ,offset.y) /TEXTURE_SIZE);
 
-        result += nearsample.rgb * kernel[i + 3];
+        result += nearsample.rgb * kernel[i + 2];
         }
         fragColor = vec4(result / kernelWeight, color.a);
     }
@@ -71,8 +73,23 @@ using namespace gl_render_utils;
 GaussianBlurPass::GaussianBlurPass(gl_render_utils::Texture *inputTexture) : mInputTexture(inputTexture) {}
 
 void GaussianBlurPass::Initialize(uint32_t width, uint32_t height) {
-    //
-    
+    //Texture may be 3684 x 1920 because it will  scaled before rendering
+
+    //Gaussian kernel 
+    const float a= 1;
+    const float b= 1;
+    const float c= 2;
+    int iwidth = 2*width;
+    int iheight =  height;
+    auto  blurCommonShaderStr = string_format(BLUR_COMMON_SHADER_FORMAT,
+                                             iwidth,
+                                             iheight,
+                                             a,
+                                             b,
+                                             c/2
+                                             );
+
+
     // Create output texture
     //mOutputTexture = make_unique<Texture>(false, 0, false, width, height);
     mOutputTexture.reset (new Texture(false, 0, false, width *2, height));
@@ -85,7 +102,7 @@ void GaussianBlurPass::Initialize(uint32_t width, uint32_t height) {
     gl_render_utils::Texture* stagTexture = mstagOutputTex1.get();
 
     // Create horizontal blur shader
-    auto horizontalBlurShader = HORIZONTAL_BLUR_SHADER;
+    auto horizontalBlurShader = blurCommonShaderStr + HORIZONTAL_BLUR_SHADER;
     mHorizontalBlurPipeline = unique_ptr<RenderPipeline>(
         new RenderPipeline(
        {mInputTexture},
@@ -93,7 +110,7 @@ void GaussianBlurPass::Initialize(uint32_t width, uint32_t height) {
         horizontalBlurShader));
   
     // Create vertical blur shader
-    auto verticalBlurShader = VERTICAL_BLUR_SHADER;
+    auto verticalBlurShader = blurCommonShaderStr + VERTICAL_BLUR_SHADER;
     mVerticalBlurPipeline = unique_ptr<RenderPipeline>(
         new RenderPipeline(
         {stagTexture},
